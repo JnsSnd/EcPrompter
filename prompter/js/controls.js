cd546d296fb909d48b2770bc1b09358709c0ff7c
Mento Bento <mbentogyo@gmail.com>
2024-11-02 15:59:19 +0800

    Update controls.js

--- prompter/js/controls.js
+++ prompter/js/controls.js
@@ -1,341 +1,154 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const prompterContainer = document.getElementById("bgPrompter");
-
     const prompterContent = document.getElementById("prompter-content");
-
-
-
     const playPauseKey = 'Space';
-
-
-
     let scrollingNow = false;
-
     let animationLoop;
-
     let fontSize;
-
-
-
-
+    let isBlackAndWhite = true;
 
     // Set the initial position of the prompter content
-
     let scrollPosition = 0;
-
     prompterContent.style.top = scrollPosition + "px";
 
-
-
     // Set the speed of auto-scrolling (adjust as needed)
-
     let scrollSpeed = 0.5;
 
-
-
     // Function to handle auto-scrolling
-
     function scrollScript() {
-
         scrollPosition -= scrollSpeed;
-
         prompterContent.style.top = scrollPosition + "px";
-
         animationLoop = requestAnimationFrame(scrollScript);
-
     }
-
-
-
+    
     function playPauseScroll() {
-
-        if (scrollingNow) {
-
-            pauseScroll();
-
-        } else {
-
-            playScroll();
-
-        }
-
+        if (scrollingNow) pauseScroll();
+        else playScroll();
     }
-
-
 
     function playScroll() {
-
         scrollingNow = true;
-
         scrollScript();
-
     }
-
-
 
     function pauseScroll() {
-
         cancelAnimationFrame(animationLoop);
-
         scrollingNow = false;
-
     }
-
-
 
     function getFontSize() {
-
         // Get the computed style of the element
-
         var computedStyle = window.getComputedStyle(prompterContent);
-
-
-
+        
         // Get the font size property from the computed style
-
         fontSize = computedStyle.getPropertyValue('font-size');
-
     }
-
-
 
     function toggleFullscreen() {
-
-        if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullscreenElement) {
-
-            exitFullscreen();
-
-        } else {
-
-            requestFullscreen();
-
-        }
-
+        if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullscreenElement) exitFullscreen();
+        else requestFullscreen();
     }
-
-
 
     function requestFullscreen() {
-
         const body = document.documentElement;
 
-
-
-        if (body.requestFullscreen) {
-
-            body.requestFullscreen();
-
-        } else if (body.webkitRequestFullscreen) {
-
-            body.webkitRequestFullscreen();
-
-        } else if (body.mozRequestFullscreen) {
-
-            body.mozRequestFullscreen();
-
-        }
-
+        if (body.requestFullscreen) body.requestFullscreen();
+        else if (body.webkitRequestFullscreen) body.webkitRequestFullscreen();
+        else if (body.mozRequestFullscreen) body.mozRequestFullscreen();
     }
 
-
-
     function exitFullscreen() {
-
-        if (document.exitFullscreen) {
-
-            document.exitFullscreen();
-
-        } else if (document.webkitExitFullscreen) {
-
-            document.webkitExitFullscreen();
-
-        } else if (document.mozCancelFullscreen) {
-
-            document.mozCancelFullscreen();
-
-        }
-
+        if (document.exitFullscreen) document.exitFullscreen();
+        else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+        else if (document.mozCancelFullscreen) document.mozCancelFullscreen();
     }
 
     var keydownListener = function (event) {
         switch (event.code) {
-
             case playPauseKey:
-
                 event.preventDefault();
-
                 playPauseScroll();
-
                 break;
-
             case 'Digit1':
-
                 event.preventDefault();
-
                 scrollSpeed = 0.5;
-
                 break;
-
             case 'Digit2':
-
                 event.preventDefault();
-
                 scrollSpeed = 1;
-
                 break;
-
             case 'Digit3':
-
                 event.preventDefault();
-
                 scrollSpeed = 1.5;
-
                 break;
-
             case 'Digit5':
-
                 event.preventDefault();
-
                 scrollSpeed = 2;
-
                 break;
-
             case 'Digit6':
-
                 event.preventDefault();
-
                 scrollSpeed = 2.5;
-
                 break;
-
             case 'Digit7':
-
                 event.preventDefault();
-
                 scrollSpeed = 3;
-
                 break;
-
             case 'Digit8':
-
                 event.preventDefault();
-
                 scrollSpeed = 3.5;
-
                 break;
-
             case 'Digit9':
-
                 event.preventDefault();
-
                 scrollSpeed = 4;
-
                 break;
-
             case 'Digit0':
-
                 event.preventDefault();
-
                 pauseScroll();
-
                 break;
-
             case 'ArrowUp':
-
                 event.preventDefault();
-
                 pauseScroll();
-
                 if (prompterContent.offsetTop >= 0) {
-
                     scrollPosition = 0;
-
                     prompterContent.style.top = "0px";
-
                 } else {
-
                     scrollPosition += 50;
-
                     prompterContent.style.top = scrollPosition + "px";
-
                 }
-
                 break;
-
             case 'ArrowDown':
-
                 event.preventDefault();
-
                 pauseScroll();
-
                 scrollPosition -= 50;
-
                 prompterContent.style.top = scrollPosition + "px";
-
                 break;
-
-
-
-            case 'F11':
-
+            case 'F11': case 'KeyF':
                 event.preventDefault();
-
                 // Toggle fullscreen when 'F' key is pressed
-
                 toggleFullscreen();
-
                 // Toggle the visibility of the element
-
                 toggleElementVisibility();
-
                 break;
-
-            
-
-            case 'KeyF':
-
-                event.preventDefault();
-
-                // Toggle fullscreen when 'F' key is pressed
-
-                toggleFullscreen();
-
-                // Toggle the visibility of the element
-
-                toggleElementVisibility();
-
-                break;
-
             case 'BracketLeft':
-
                 event.preventDefault();
                 var currentSize = window.getComputedStyle(prompterContent).fontSize;
                 var newSize = parseInt(currentSize) - 2;
                 prompterContent.style.fontSize = newSize + 'px';
-
                 sessionStorage.setItem('fontSize', newSize);
-
                 break;
-
             case 'BracketRight':
-
                 event.preventDefault();
                 var currentSize = window.getComputedStyle(prompterContent).fontSize;
                 var newSize = parseInt(currentSize) + 2;
                 prompterContent.style.fontSize = newSize + 'px';
-
                 sessionStorage.setItem('fontSize', newSize);
-
                 break;
-
             case 'KeyP':
-
                 event.preventDefault();
+                if {isBlackAndWhite} localStorage.setItem('prompterType', 'LYRICS_PROMPTER');
+                else localStorage.setItem('prompterType', 'BLACK_AND_WHITE');
                 prompterContainer.classList.toggle('lyricsPrompter');
                 prompterContainer.classList.toggle('blackwhite');
-                localStorage.setItem('prompterType', 'LYRICS_PROMPTER');
-
                 break;
         }
     };
@@ -351,7 +164,6 @@ document.addEventListener("DOMContentLoaded", function () {
     notEditable();
     function notEditable() {
         document.addEventListener('keydown', keydownListener);
-
     }
 
     function Editable() {
@@ -407,5 +219,4 @@ document.addEventListener("DOMContentLoaded", function () {
         document.execCommand('styleWithCSS', false, true);
         document.execCommand('foreColor', false, color);
     }
-
 });
